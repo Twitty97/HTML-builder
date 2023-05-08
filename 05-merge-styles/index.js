5b08c725f1bec4db426e99b8bf2db3ba17dc166f
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-05-09 00:43:38 +0500

    refactor(05-merge-styles): convert mergeStyles function into a module to be able to reuse outside of the current task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,28 +5,34 @@ const dirPath = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(dirPath, 'bundle.css');
 const stylesPath = path.join(__dirname, 'styles');
 
-fs.readdir(stylesPath, 'utf-8', (err, files) => {
-  if (err)
-    console.log(err);
-  else {
-    files.forEach(file => {
-      const filePath = path.join(stylesPath, file);
-      const ext = path.extname(filePath);
-      if(ext === '.css') {
-        const readStream = fs.createReadStream(filePath);
-        readStream.on('data', (data) => {
-          fs.appendFile(bundlePath, data, (err) => {
-            if (err) {
-              console.log(err);
-            }
+function mergeStyles(stylesPath) {
+  fs.readdir(stylesPath, 'utf-8', (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        const filePath = path.join(stylesPath, file);
+        const ext = path.extname(filePath);
+        if(ext === '.css') {
+          const readStream = fs.createReadStream(filePath);
+          readStream.on('data', (data) => {
+            fs.appendFile(bundlePath, data, (err) => {
+              if (err) {
+                console.log(err);
+              }
+            });
           });
-        });
-      }
-    });
-    fs.writeFile(bundlePath, '', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+        }
+      });
+      fs.writeFile(bundlePath, '', (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  });
+}
+
+mergeStyles(stylesPath);
+
+module.exports = mergeStyles;
