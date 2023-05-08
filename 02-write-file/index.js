f644e1c8c9a395e3685f73674c323d61469c1d24
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-05-08 13:53:49 +0500

    refactor(02-write-file): add a goodbye message when exit entered or hot keys are pressed

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,6 +9,7 @@ const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
 process.stdin.on('data', data => {
   const input = data.toString().trim();
   if (input === 'exit') {
+    process.stdout.write('Thank u! Goodbye!\n');
     process.exit();
   } else {
     writeStream.write(input + '\n');
@@ -16,5 +17,6 @@ process.stdin.on('data', data => {
   }
 });
 process.on('SIGINT', function() {
+  console.log('Thank u! Goodbye!\n');
   process.exit();
 });
