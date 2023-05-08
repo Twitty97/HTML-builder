b7bb3fd1e1606d003692901a5e25da58074e709f
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-05-08 14:14:23 +0500

    fix(03-files-in-folder): remove the .ext from file name when displaying contents of the directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const dirPath = require('path');
-
 const folderPath = dirPath.join(__dirname, 'secret-folder');
 
 fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
-  console.log('\nCurrent directory files:');
+  console.log('\nCurrent directory files:\n');
   if (err)
     console.log(err);
   else {
@@ -13,10 +12,10 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
       const ext = dirPath.extname(file.name).substring(1);
       fs.stat(filePath, (err, stats) => {
         if(stats.isFile()) {
-          console.log(`${file.name} - ${ext} - ${parseFloat((stats.size/1000))}kb`);
+          const noExt = file.name.slice(0, file.name.indexOf('.'));
+          console.log(`${noExt} - ${ext} - ${parseFloat((stats.size/1000))}kb`);
         }
       });
     });
   }
 });
-
