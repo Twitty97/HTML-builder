4c3a2f8df2ad7b68e8b61c7036d33b3475a194e7
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-05-09 00:26:42 +0500

    feat: add function to merge styles (solution to task 05-merge-styles)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, 'project-dist');
+const bundlePath = path.join(dirPath, 'bundle.css');
+const stylesPath = path.join(__dirname, 'styles');
+
+fs.readdir(stylesPath, 'utf-8', (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      const filePath = path.join(stylesPath, file);
+      const ext = path.extname(filePath);
+      if(ext === '.css') {
+        const readStream = fs.createReadStream(filePath);
+        readStream.on('data', (data) => {
+          fs.appendFile(bundlePath, data, (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+        });
+      }
+    });
+    fs.writeFile(bundlePath, '', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
