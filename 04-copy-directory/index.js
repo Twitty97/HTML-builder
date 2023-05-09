83b49a16d4539dc7769747fb700629407c253b10
Twitty97 <farangiz.nurmukhamedova97@gmail.com>
2023-05-09 17:19:10 +0500

    fix: 04-copy-directory problem with the directory not being removed on delete origin

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,8 +10,25 @@ fs.mkdir(`${__dirname}/files-copy`, { recursive: true }, (err) => {
 const source = path.join(__dirname, 'files');
 const destination = path.join(__dirname, 'files-copy');
 
+
+// clean the directory before copying 
+cleanDirectory(destination);
 // create a copy of all files inside the directory
 copyFile(source, destination);
+
+function cleanDirectory(destination) {
+  fs.readdir(destination, { withFileTypes: true }, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.unlink(path.join(destination, file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    }
+  });
+}
 
 function copyFile(source, destination) {
   fs.readdir(source, { withFileTypes: true }, (err, files) => {
@@ -39,5 +56,3 @@ function copyFile(source, destination) {
 function callback(err) {
   if (err) throw err;
 }
-
-module.exports = copyFile;
